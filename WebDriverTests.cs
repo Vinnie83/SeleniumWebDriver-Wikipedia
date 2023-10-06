a8863d5da2966cabc0e4d42e63cf8cdf0accbdf9
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-10-06 14:29:28 +0300

    Add project files.

---
+++ WebDriverTests.cs
@@ -0,0 +1,72 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumWebDriverNunitDemo
+{
+    public class WebDriverTests
+    {
+
+        private WebDriver driver;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [OneTimeTearDown]
+        public void CloseBrowser()
+        {
+            this.driver.Quit();
+        }
+
+        [Test]
+        public void Test_Wikipedia_CheckTitle()
+        {
+            driver.Url = "https://wikipedia.org";
+
+            var pageTitle = driver.Title;
+
+            Assert.That("Wikipedia",Is.EqualTo(pageTitle));
+        }
+
+        [Test]
+        public void Test_Wikipedia_SearchField()
+        {
+            driver.Url = "https://wikipedia.org";
+
+            var searchField = driver.FindElement(By.Id("searchInput"));
+            searchField.SendKeys("QA" + Keys.Enter);
+
+            var pageTitle = driver.Title;
+
+            Assert.That("QA - Wikipedia", Is.EqualTo(pageTitle));
+        }
+
+        [Test]
+        public void Test_Wikipedia_CheckDeutsch()
+        {
+            driver.Url = "https://wikipedia.org";
+
+            var allStrongElements = driver.FindElements(By.TagName("strong"));
+
+            var deutschLinkText = allStrongElements[5].Text;
+
+            Assert.That("Deutsch", Is.EqualTo(deutschLinkText));
+        }
+
+        [Test]
+        public void Test_Wikipedia_CheckEnglish()
+        {
+            driver.Url = "https://wikipedia.org";
+
+            var allStrongElements = driver.FindElements(By.TagName("strong"));
+
+            var englishLinkText = allStrongElements[1].Text;
+
+            Assert.That("English", Is.EqualTo(englishLinkText));
+        }
+
+    }
+}
